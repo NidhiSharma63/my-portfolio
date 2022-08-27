8cb5f4f1fbc980397655507d37ff93321f491601
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-08-27 08:48:02 +0530

    about section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './assets/css/main.css';
 import Header from './components/Header';
 import Herobanner from './components/Herobanner';
-
+import About from './components/About';
 
 const App = () => {
   return (
     <div className='main-wrapper'>
       <Header/>
       <Herobanner/>
+      <About/>
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import aboutImg from '../assets/images/about.svg'
+
+const About = () => {
+  return (
+    <div className='container-2'>
+      <div className="about-wrapper">
+        <div className="left-section">
+          <p>About me</p>
+          <img src={aboutImg} alt="about" />
+        </div>
+        <div className="right-section">
+          <div className="top-shadow"></div>
+          <div className="bottom-shadow"></div>
+
+          <p>I am a self-taught frontend web-developer in india with a great passion of creating beautiful responsive websites. currently,I am looking for internship and want to help business.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About;
