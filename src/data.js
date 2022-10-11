cc9a8023982aef6546b1967fbfd88cbf9c50aaa8
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-10-11 07:57:48 +0530

    project paragraph text updated

--- src/data.js
+++ src/data.js
@@ -16,10 +16,7 @@ export const projectData = [
     image1:`${pf1}`,
     image2:`${pf2}`,
     projectName:`Portfolio`,
-    projectDesc:`i created this portfolio for my client
-    and it is entriley responsive. I have 
-    created this portfolio by using
-    react and material ui.`,
+    projectDesc:`I created this portfolio for my client and it is entirely responsive. I have created this portfolio by using React and Material UI.`,
     live:'https://nidhisharma63.github.io/client-portfolio/',
     github:'https://github.com/NidhiSharma63/client-portfolio'
   },
@@ -27,7 +24,7 @@ export const projectData = [
     image1:`${gm1}`,
     image2:`${gm2}`,
     projectName:`Good Meeting-clone`,
-    projectDesc:`This is a clone of goodmeeting.today. i used html, css, javascript to clone it. it's completely responsive.`,
+    projectDesc:`This is a clone of GoodMeeting. today. I used html, CSS, and Javascript to clone it. It's completely responsive.`,
     live:'https://nidhisharma63.github.io/goodmeeting-clone/',
     github:'https://master--kaleidoscopic-sprinkles-74d6ee.netlify.app/'
   },
@@ -45,8 +42,7 @@ export const projectData = [
     image1:`${car1}`,
     image2:`${car2}`,
     projectName:`Car game`,
-    projectDesc:`This is a car game that I built with HTML, CSS, and JavaScript.
-    by using arrow keys you can control the car. it's not responsive`,
+    projectDesc:`This is a car game that I built with HTML, CSS, and JavaScript. By using the arrow keys, you can control the car. It's not responsive.`,
     live:'https://nidhisharma63.github.io/Car_game/',
     github:'https://github.com/NidhiSharma63/Car_game'
   },
