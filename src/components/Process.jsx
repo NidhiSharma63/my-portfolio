1d999ea092b4a2391f8a8511d15454362143551b
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2022-12-27 20:19:54 +0530

    text centered

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,29 +1,30 @@
-import React,{useEffect} from 'react';
-import wireFrame from '../assets/images/wfPC.jpg';
-import wireFrame2 from '../assets/images/wfMB.jpg';
-import { translateAnim } from '../assets/js/main';
+import React, { useEffect } from "react";
+import wireFrame from "../assets/images/wfPC.jpg";
+import wireFrame2 from "../assets/images/wfMB.jpg";
+import { translateAnim } from "../assets/js/main";
 
 const Process = () => {
-
   let container4 = {
-    selector:'container-4',
-    classes:'translate-up'
-  }
-  useEffect(()=>{
+    selector: "container-4",
+    classes: "translate-up",
+  };
+  useEffect(() => {
     translateAnim(container4);
-  },[]);
+  }, []);
 
   return (
     <div className="container-4 translate-up">
       <div className="process-wrapper">
         <div className="top-section">
-          <p>Approach i generally used</p>
-          <p>I like to use wireframes to make responsive websites. It helps me to get a clear idea of what I have to do.
+          <p>Approach, I generally used</p>
+          <p>
+            I like to use wireframes to make responsive websites. It helps me to
+            get a clear idea of what I have to do.
           </p>
         </div>
         <div className="bottom-section">
           <div className="image">
-            <img src={wireFrame} alt="wireFrame"/>
+            <img src={wireFrame} alt="wireFrame" />
           </div>
           <div className="image">
             <img src={wireFrame2} alt="wireFrame" />
@@ -31,7 +32,7 @@ const Process = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Process;
