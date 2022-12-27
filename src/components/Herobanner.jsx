0bf134485d30df70664968db8e170440c90b7921
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2022-12-27 16:50:17 +0530

    herobanner imaged added

--- src/components/Herobanner.jsx
+++ src/components/Herobanner.jsx
@@ -1,56 +1,68 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from "react";
 
-import mainImg from '../assets/images/main-img.svg';
-import { translateAnim } from '../assets/js/main';
+// import mainImg from '../assets/images/main-img.svg';
+import mainImg from "../assets/images/Edited/my3.jpg";
+
+import { translateAnim } from "../assets/js/main";
 
 const Herobanner = () => {
   let Image = {
-    selector:'right-section',
-    classes:'translate-right'
-  }
+    selector: "right-section",
+    classes: "translate-right",
+  };
 
-  
-  useEffect(()=>{
+  useEffect(() => {
     translateAnim(Image);
-  },[])
+  }, []);
 
   return (
-    <div className='wrapper-1' id='Home'>
+    <div className="wrapper-1" id="Home">
       <div className="left-shadow"></div>
       <div className="hero-banner-wrapper">
         <div className="left-section">
           <p>Hy, I Am</p>
           <p>Nidhi Sharma</p>
-          <p>A frontend developer who turns imagination into reality by using code and loves to listen to music </p>
+          <p>
+            A frontend developer who turns imagination into reality by using
+            code and loves to listen to music 
+          </p>
           <div className="hero-btn-wrapper">
-          <button>
-            <a href="#Work">Checkout my work</a>
-          </button>
-          <button>
-            <a target='blank' href="https://drive.google.com/file/d/1iCy2lPL_2r80uX0ej2DbIU6dS6AhB0P1/view?usp=sharing">Get resume</a>
-          </button>
+            <button>
+              <a href="#Work">Checkout my work</a>
+            </button>
+            <button>
+              <a
+                target="blank"
+                href="https://drive.google.com/file/d/1iCy2lPL_2r80uX0ej2DbIU6dS6AhB0P1/view?usp=sharing"
+              >
+                Get resume
+              </a>
+            </button>
           </div>
         </div>
         <div className="right-section translate-right">
-          <img src={mainImg} alt="image" />
-          <div className='hero-icons'>
-            <a target='blank' href="https://medium.com/@nidhisharma639593">
+          <img src={mainImg} alt="It's me" />
+          <div className="hero-icons">
+            <a target="blank" href="https://medium.com/@nidhisharma639593">
               <i className="fa-brands fa-medium"></i>
             </a>
-            <a target='blank' href="https://github.com/NidhiSharma63">
+            <a target="blank" href="https://github.com/NidhiSharma63">
               <i className="fa-brands fa-square-github"></i>
             </a>
-           <a target='blank' href="https://www.linkedin.com/in/nidhi-sharma-55329823b/">
-            <i className="fa-brands fa-linkedin"></i>
-           </a>
-           <a target='blank' href="https://twitter.com/NidhiSh57914602">
-             <i className="fa-brands fa-square-twitter"></i>
-           </a>
+            <a
+              target="blank"
+              href="https://www.linkedin.com/in/nidhi-sharma-55329823b/"
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a target="blank" href="https://twitter.com/NidhiSh57914602">
+              <i className="fa-brands fa-square-twitter"></i>
+            </a>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Herobanner
+export default Herobanner;
