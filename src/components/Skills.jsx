4b5f86ea59ff900f94fc33feda6e891c49794e3c
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2022-12-30 17:08:10 +0530

    skill: heading fixed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,42 +1,38 @@
-import React,{ useEffect } from 'react';
-import { translateAnim } from '../assets/js/main';
-
+import React, { useEffect } from "react";
+import { translateAnim } from "../assets/js/main";
 
 const Skills = () => {
-
   let container3 = {
-    selector:'container-3',
-    classes:'translate-up'
-  }
-  useEffect(()=>{
+    selector: "container-3",
+    classes: "translate-up",
+  };
+  useEffect(() => {
     translateAnim(container3);
-  },[]);
+  }, []);
 
   return (
-    <div className='container-3 translate-up'>
-    <div className="right-shadow"></div>
+    <div className="container-3 translate-up">
+      <div className="right-shadow"></div>
       <div className="skill-wrapper">
-        
         <div className="left-shadow"></div>
         <div className="left-section">
-          <div className='skill-icon'></div>
-          <div className='skill-icon'></div>
-          <div className='skill-icon'></div>
-          <div className='skill-icon'></div>
-          <div className='skill-icon'></div>
-          <div className='skill-icon'></div>
+          <div className="skill-icon"></div>
+          <div className="skill-icon"></div>
+          <div className="skill-icon"></div>
+          <div className="skill-icon"></div>
+          <div className="skill-icon"></div>
+          <div className="skill-icon"></div>
         </div>
         <div className="right-section">
-          <div className="heading">
-            Here are my<br/> skills
-          </div>
+          <div className="heading">Here are my skills</div>
           <div className="skills-info">
-            My skills are JavaScript, HTML, and CSS. I have learned frameworks like Bootstrap,Material UI, Tailwind, and also Git and Figma.
+            My skills are JavaScript, HTML, and CSS. I have learned frameworks
+            like Bootstrap,Material UI, Tailwind, and also Git and Figma.
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Skills;
