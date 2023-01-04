b6661723a7d6304323bf511876b98fc1f82e20d9
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-01-04 20:23:14 +0530

    react-router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import './assets/css/main.css';
-import Header from './components/Header';
-import Herobanner from './components/Herobanner';
-import About from './components/About';
-import Skills from './components/Skills';
-import Process from './components/Process';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import React from "react";
+import "./assets/css/main.css";
+import Header from "./components/Header";
+import Herobanner from "./components/Herobanner";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Process from "./components/Process";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className='main-wrapper'>
-      <Header/>
-      <Herobanner/>
-      <About/>
-      <Skills/>
-      <Process/>
-      <Work/>
-      <Contact/>
+    <div className="main-wrapper">
+      <Header />
+      <Herobanner />
+      <About />
+      <Skills />
+      <Process />
+      <Work />
+      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
