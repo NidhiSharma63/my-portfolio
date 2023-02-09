23f233c5f408bd0ae073fd541681c2a66ad6ad8f
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-02-09 16:24:23 +0530

    share link removed

--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -11,133 +11,19 @@ import BlogHeaderAndFooter from "components/blogs/BlogHeaderAndFooter";
 import LinkModal from "./Portal";
 import useFetchData from "hooks/useFecthData";
 
-/**get ip address */
-
-const getIpAddress = async () => {
-  let ipAddress;
-  try {
-    const response = await fetch("https://api.ipify.org?format=json");
-    const data = await response.json();
-    ipAddress = data.ip;
-  } catch (error) {
-    console.error(error);
-  }
-
-  return ipAddress;
-};
-
 const Blog = () => {
   const { blogs } = useFetchData();
-  let newIpAddress;
-  // const [userIpAddress, setUserIpAddress] = useState("");
-  let userIpAddress;
   const [selectedBlog, setSelectedBlog] = useState([]);
   const [blogId, setBlogId] = useState(JSON.parse(getValueFromLS("blogId")));
-  const [likedCount, setLikedCount] = useState(
-    selectedBlog && selectedBlog.data ? selectedBlog.data.likedNum : 0
-  );
-
-  const [sharelink, setShareLink] = useState();
-  const [isLiked, setIsLiked] = useState(false);
 
   /** run hook to find the selected blog */
   useEffect(() => {
     Object.entries(blogs || {}).map((blog) => {
       if (blog[0] === blogId) {
         setSelectedBlog(blog[1]);
-        setLikedCount(blog[1]?.data?.likedNum);
       }
     });
   }, [blogs, blogId]);
-
-  const checkIfUserlikedTheBlog = async () => {
-    try {
-      const response = await fetch("https://api.ipify.org/?format=json");
-      const data = await response.json();
-      newIpAddress = data.ip;
-    } catch (error) {
-      console.error(error);
-    }
-
-    console.log(
-      selectedBlog?.data?.UserIpAddress,
-      "selectedBlog?.data?.UserIpAddress"
-    );
-
-    console.log(newIpAddress, "newIpAddress");
-
-    if (selectedBlog?.data?.UserIpAddress === newIpAddress) {
-      setIsLiked(true);
-    }
-  };
-
-  useEffect(() => {
-    checkIfUserlikedTheBlog();
-  }, [selectedBlog]);
-
-  // /** Increase the like */
-  const handleUnLikedHeartClick = async () => {
-    setIsLiked(true);
-
-    try {
-      const response = await fetch("https://api.ipify.org/?format=json");
-      const data = await response.json();
-      userIpAddress = data.ip;
-    } catch (error) {
-      console.error(error);
-    }
-
-    // update firebase
-    update(ref(db, `${process.env.REACT_APP_AUTHKEY}/${blogId}`), {
-      data: {
-        id: blogId,
-        title: selectedBlog?.data?.title,
-        summary: selectedBlog?.data?.summary,
-        body: selectedBlog?.data?.body,
-        likedNum: likedCount + 1,
-        UserIpAddress: userIpAddress,
-      },
-    });
-    // updateing like
-    setLikedCount((prev) => prev + 1);
-  };
-
-  // /** Decrease the like */
-  const handleLikedHeartClick = () => {
-    setIsLiked(false);
-    // update firebase
-    update(ref(db, `${process.env.REACT_APP_AUTHKEY}/${blogId}`), {
-      data: {
-        id: blogId,
-        title: selectedBlog?.data?.title,
-        summary: selectedBlog?.data?.summary,
-        body: selectedBlog?.data?.body,
-        likedNum: likedCount - 1,
-        UserIpAddress: "",
-      },
-    });
-
-    // updateing like
-    setLikedCount((prev) => prev - 1);
-  };
-
-  // /** handle node click  show the links modal*/
-  const handelNodeClick = () => {
-    setShareLink((prev) => (prev = !prev));
-  };
-
-  /** handleTwitterClick generate the twitter link */
-  const handleTwitterClick = () => {
-    const url =
-      "https://twitter.com/intent/tweet?text=http%3A//localhost%3A3000/blog/Bageshwar";
-    window.open(url, "_blank");
-  };
-  /** handleLinkedInClick generate the linkedin link */
-  const handleLinkedInClick = () => {
-    const url =
-      "https://www.linkedin.com/shareArticle?mini=true&url=http%3A//localhost%3A3000/blog/Bageshwar";
-    window.open(url, "_blank");
-  };
 
   /** submit comment */
 
@@ -158,26 +44,7 @@ const Blog = () => {
             key={uuidv4()}
           />
         </div>
-        <div className="specific-blog-icons-container">
-          {isLiked ? (
-            <i
-              className="fa-solid fa-heart heat-red"
-              onClick={() => handleLikedHeartClick(selectedBlog?.data?.id)}
-            ></i>
-          ) : (
-            <i
-              className="fa-regular fa-heart"
-              onClick={() => handleUnLikedHeartClick(selectedBlog?.data?.id)}
-            ></i>
-          )}
-          <p>{likedCount}</p>
-          <div>
-            <i
-              className="fa-solid fa-share-nodes"
-              onClick={handelNodeClick}
-            ></i>
-          </div>
-        </div>
+
         <div className="about-author">
           <div className="col-1">
             <div className="autho-img">
@@ -201,50 +68,9 @@ const Blog = () => {
             skills and knowledge to real-world projects.
           </p>
         </div>
-        {sharelink ? (
-          <LinkModal>
-            <p>Share this article with</p>
-            <div>
-              <i
-                className="fa-brands fa-twitter"
-                onClick={handleTwitterClick}
-              ></i>
-              <i
-                className="fa-brands fa-linkedin"
-                onClick={handleLinkedInClick}
-              ></i>
-            </div>
-          </LinkModal>
-        ) : null}
       </BlogHeaderAndFooter>
     </div>
   );
 };
 
 export default Blog;
-
-// const [showBlog, setShowBlog] = useState([
-//   JSON.parse(getValueFromLS("blog")),
-// ]);
-// const [sharelink, setShareLink] = useState(false);
-// const [isLiked, setIsLiekd] = useState(
-//   // JSON.parse(getValueFromLS("isLikedBlog")) ?? false
-//   showBlog[0]?.data?.likedNum > 0
-// );
-// console.log(showBlog[0]?.data?.likedNum > 0, "json value");
-
-// document.body.addEventListener("click", (e) => {
-//   if (
-//     e.target.className.includes("fa-share-nodes") ||
-//     e.target.className.includes("fa-twitter") ||
-//     e.target.className.includes("fa-linkedin")
-//   ) {
-//     setShareLink(true);
-//     return;
-//   } else {
-//     setShareLink(false);
-//   }
-// });
-// const [likedCount, setLikedCount] = useState(
-//   showBlog[0]?.data?.likedNum ?? 0
-// );
