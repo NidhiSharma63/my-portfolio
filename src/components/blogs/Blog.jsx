351ca9c0c0cace14c9ffe37229958c6407210ae1
nidhi.sharma@dronebase.com <nidhi.sharma@dronebase.com>
2023-02-13 12:56:29 +0530

    blog page helmet tag shift up

--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -25,7 +25,8 @@ const Blog = () => {
   /** submit comment */
 
   return (
-    <div className="main-wrapper">
+    <>
+      {" "}
       <Helmet>
         <title>{selectedBlog?.data?.title}</title>
         <meta
@@ -33,40 +34,43 @@ const Blog = () => {
           content={selectedBlog?.data?.summary.replace(/<img.*?>/g, "")}
         />
       </Helmet>
-      <BlogHeaderAndFooter>
-        <div className="specific-blog-container">
-          <MarkdownLib
-            className={"specific-blog"}
-            markdown={selectedBlog?.data?.body}
-            key={uuidv4()}
-          />
-        </div>
-
-        <div className="about-author">
-          <div className="col-1">
-            <div className="autho-img">
-              <img src={mainImg} alt="author" />
-            </div>
-            <div className="upper-text">
-              <strong>WRITTEN BY</strong>
-              <p>Nidhi Sharma</p>
-            </div>
+      <div className="main-wrapper">
+        <BlogHeaderAndFooter>
+          <div className="specific-blog-container">
+            <MarkdownLib
+              className={"specific-blog"}
+              markdown={selectedBlog?.data?.body}
+              key={uuidv4()}
+            />
           </div>
-          <p className="col-2">
-            Hi there! My name is Nidhi sharma and I am a frontend developer
-            currently working as an intern. I have a strong passion for creating
-            user-friendly and visually appealing websites and applications, and
-            I am always looking to learn and improve my skills. As a frontend
-            developer, I use a variety of programming languages and tools, such
-            as HTML, CSS, and JavaScript, React, to bring my designs to life and
-            make them interactive and responsive. I am excited to have the
-            opportunity to learn from and contribute to a team of experienced
-            developers. I am eager to take on new challenges and to apply my
-            skills and knowledge to real-world projects.
-          </p>
-        </div>
-      </BlogHeaderAndFooter>
-    </div>
+
+          <div className="about-author">
+            <div className="col-1">
+              <div className="autho-img">
+                <img src={mainImg} alt="author" />
+              </div>
+              <div className="upper-text">
+                <strong>WRITTEN BY</strong>
+                <p>Nidhi Sharma</p>
+              </div>
+            </div>
+            <p className="col-2">
+              Hi there! My name is Nidhi sharma and I am a frontend developer
+              currently working as an intern. I have a strong passion for
+              creating user-friendly and visually appealing websites and
+              applications, and I am always looking to learn and improve my
+              skills. As a frontend developer, I use a variety of programming
+              languages and tools, such as HTML, CSS, and JavaScript, React, to
+              bring my designs to life and make them interactive and responsive.
+              I am excited to have the opportunity to learn from and contribute
+              to a team of experienced developers. I am eager to take on new
+              challenges and to apply my skills and knowledge to real-world
+              projects.
+            </p>
+          </div>
+        </BlogHeaderAndFooter>
+      </div>
+    </>
   );
 };
 
