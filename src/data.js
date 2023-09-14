ac77ac87dec1c76429f1d8d485fe9429b7e37f30
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-09-14 07:34:44 +0530

    youtube clone added

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import tmgif from "./assets/images/tm.gif";
 import cggif from "./assets/images/cg.gif";
-import cpgif from "./assets/images/cp.gif";
+import youtube from "./assets/images/youtube .gif";
 import exgif from "./assets/images/ex.gif";
 
 export const projectData = [
@@ -13,19 +13,17 @@ export const projectData = [
   },
   {
     image1: `${cggif}`,
-    // image2: `${car2}`,
     projectName: `Car game`,
     projectDesc: `This is a car game that I built with HTML, CSS, and JavaScript. By using the arrow keys, you can control the car. It's not responsive.`,
     live: "https://nidhisharma63.github.io/Car_game/",
     github: "https://github.com/NidhiSharma63/Car_game",
   },
   {
-    image1: `${cpgif}`,
-    // image2: `${pf2}`,
-    projectName: `Portfolio`,
-    projectDesc: `I created this portfolio for my client and it is entirely responsive. I have created this portfolio by using React and Material UI.`,
-    live: "https://nidhisharma63.github.io/client-portfolio/",
-    github: "https://github.com/NidhiSharma63/client-portfolio",
+    image1: `${youtube}`,
+    projectName: `Youtube Clone`,
+    projectDesc: `I created this app using React React-Player Rapid API TypeScript. Here you can search the videos, and can save into playlist or watchlater`,
+    live: "https://nidhisharma63.github.io/youtube-clone/",
+    github: "https://github.com/NidhiSharma63/youtube-clone",
   },
 
   {
