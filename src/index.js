eea693f5be6f842d779141a64cc8d0d3c3601ec9
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-01-14 18:06:19 +0530

    sop added

--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
+import HomePage from "pages/HomePage";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HomePage from "pages/HomePage";
-import "./assets/css/main.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import APP_ENDPOINTS from "./constant/App_And_Point";
-import MarkDown from "./components/Markdown/Markdown";
-import Errorpage from "./components/Notfound/Errorpage";
-import Blog from "components/blogs/Blog";
-import { Provider } from "react-redux";
+import {HelmetProvider} from "react-helmet-async";
+import {Provider} from "react-redux";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import store from "store/store";
-import { getValueFromLS } from "utlis/Localstorage";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import "./assets/css/main.css";
+import Errorpage from "./components/Notfound/Errorpage";
+import APP_ENDPOINTS from "./constant/App_And_Point";
 
-const rootElement = document.getElementById("root");
+// Check if your app is being displayed in an iframe
+if (window !== window.top) {
+  // If it's in an iframe, redirect to your app's URL
+  window.top.location.href = window.location.href;
+} else {
+  const rootElement = document.getElementById("root");
+  const root = ReactDOM.createRoot(rootElement);
+  const router = createBrowserRouter([
+    {
+      path: APP_ENDPOINTS.My_PORTFOLIO || APP_ENDPOINTS.ROOT,
+      element: (
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      ),
+    },
+    {
+      path: APP_ENDPOINTS.ROOT,
+      element: (
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      ),
+    },
+    {
+      path: "*",
+      element: <Errorpage />,
+    },
+  ]);
 
-const root = ReactDOM.createRoot(rootElement);
-
-const router = createBrowserRouter([
-  {
-    path: APP_ENDPOINTS.My_PORTFOLIO || APP_ENDPOINTS.ROOT,
-    element: (
-      <Provider store={store}>
-        <HomePage />
-      </Provider>
-    ),
-  },
-  {
-    path: APP_ENDPOINTS.ROOT,
-    element: (
-      <Provider store={store}>
-        <HomePage />
-      </Provider>
-    ),
-  },
-
-  {
-    path: "*",
-    element: <Errorpage />,
-  },
-]);
-
-root.render(
-  <HelmetProvider>
-    <RouterProvider router={router} />
-  </HelmetProvider>
-);
+  root.render(
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
+}
