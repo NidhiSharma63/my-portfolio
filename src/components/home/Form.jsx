b4349bbb9c7769cbebd1028e3759bfd42765c8da
NidhiSharma63 <nidhisharma639593@gmail.com>
2024-01-15 13:33:31 +0530

    email.js added in csp

--- src/components/home/Form.jsx
+++ src/components/home/Form.jsx
@@ -1,20 +1,20 @@
-import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import FormikInput from "components/formik/FormikInput";
-import { Formik, Form } from "formik";
-import { ClientValidation } from "constant/validation";
+import {ClientValidation} from "constant/validation";
+import {Form, Formik} from "formik";
 import useInitialValues from "hooks/useInitialValues";
+import React, {useRef} from "react";
 
-const FormComponent = ({ setEmailConfirmation }) => {
+const FormComponent = ({setEmailConfirmation}) => {
   const formRef = useRef(null);
-  const { initialValues } = useInitialValues();
+  const {initialValues} = useInitialValues();
   const handleEmailSubmit = (value) => {
     emailjs
       .sendForm(
-        "service_g5wb7nq",
-        "template_rwo6xed",
+        process.env.REACT_APP_SERVICE,
+        process.env.REACT_APP_TEMPLATE,
         formRef.current,
-        "Nw9mYKs-BMJleDqIB"
+        process.env.REACT_APP_KEY
       )
       .then(() => {
         setEmailConfirmation(true);
@@ -22,19 +22,11 @@ const FormComponent = ({ setEmailConfirmation }) => {
   };
   return (
     <div className="middle-section">
-      <Formik
-        validationSchema={ClientValidation}
-        onSubmit={handleEmailSubmit}
-        initialValues={initialValues}
-      >
+      <Formik validationSchema={ClientValidation} onSubmit={handleEmailSubmit} initialValues={initialValues}>
         <Form ref={formRef}>
           <FormikInput name="clientName" placeholder="john" />
           <FormikInput name="email" placeholder="john@gmail.com" />
-          <FormikInput
-            name="message"
-            component="textarea"
-            placeholder="Hey! Nidhi...."
-          />
+          <FormikInput name="message" component="textarea" placeholder="Hey! Nidhi...." />
           <button type="submit">send</button>
         </Form>
       </Formik>
